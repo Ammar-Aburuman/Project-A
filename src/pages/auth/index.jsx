a21adb5a140b7d5eb0b351f8ece7003bffdc977d
Ammar Aburuman <A.aburuman@protonmail.com>
2024-07-04 13:45:08 +0300

    add google auth to website

---
+++ src/pages/auth/index.jsx
@@ -0,0 +1,17 @@
+import {auth, provider} from "../../config/firebase-config"
+import { signInWithPopup } from "firebase/auth"
+
+export const Auth = () => {
+
+    const signInWithGoogle = async () => {
+        const results = await signInWithPopup(auth,provider)
+        console.log(results)
+    }
+
+    return (
+        <div className="login-page">
+            <p>Sign in with Google</p>
+            <button className="login-with-google-btn" onClick={signInWithGoogle}>Sign in with Google </button>
+        </div>
+    );
+}

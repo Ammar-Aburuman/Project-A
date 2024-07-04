14efb0e4713960e60327f17d6ab56e13092277bd
Ammar Aburuman <A.aburuman@protonmail.com>
2024-07-04 16:07:38 +0300

    add functioning form for data entry into database

--- src/pages/expense-tracker/index.jsx
+++ src/pages/expense-tracker/index.jsx
@@ -1,5 +1,21 @@
+import { useState } from "react";
+import { useAddTransaction } from "../../hooks/useAddTransactions";
+
 export const ExpenseTracker = () => {
+
+    const {addTransaction} = useAddTransaction();
+
+    const [descrption,setDescription] = useState("");
+    const [transactionAmount,setTransactionAmount] = useState(0);
+    const [transactionType,setTransactionType] = useState("expense");
+
+    const onSubmit = async (e) => {
+        e.preventDefault() //prevents page refresh on form submission
+        addTransaction({descrption, transactionAmount, transactionType})
+    };
+
     return (
+        <>
         <div className="expense-tracker">
 
             <div className="container">
@@ -18,8 +34,28 @@ export const ExpenseTracker = () => {
                     <p>$0.00</p>
                     </div>
                 </div>
+                <form className="add-transaction" onSubmit={onSubmit}>
+                    <input type="text" placeholder="Description" required onChange={(e) => setDescription(e.target.value)}/>
+
+                    <input type="number" placeholder="Amount" required onChange={(e) => setTransactionAmount(e.target.value)}/>
+
+                    <input type="radio" id="expense" value="expense" checked ={transactionType === "expense"} required onChange={(e) => setTransactionType(e.target.value)}/>
+
+                    <label htmlFor="expense">Expense</label>
+                    
+
+                    <input type="radio" id="income" value="income" checked ={transactionType === "income"} required onChange={(e) => setTransactionType(e.target.value)}/>
+
+                    <label htmlFor="income">Income</label>
+
+                    <button type="submit">Add Transaction</button>
+                </form>
             </div>
 
         </div>
+        <div className="transactions">
+            <h3>Transactions</h3>
+        </div>
+        </>
     );
 }

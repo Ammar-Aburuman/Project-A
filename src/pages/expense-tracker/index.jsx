2beabb843a015437b2149bd47f2d158285168e04
Ammar Aburuman <A.aburuman@protonmail.com>
2024-07-04 14:28:41 +0300

    add route to expense-tracker website + fix css

--- src/pages/expense-tracker/index.jsx
+++ src/pages/expense-tracker/index.jsx
@@ -1,3 +1,25 @@
 export const ExpenseTracker = () => {
-    return <div>Expense-Tracker</div>
+    return (
+        <div className="expense-tracker">
+
+            <div className="container">
+                <h1>Expense Tracker</h1>
+                <div className="balance">
+                    <h3>Your balance</h3>
+                    <h2>$0.00</h2>
+                </div>
+                <div className="summary">
+                    <div className="income">
+                        <h4>Income</h4>
+                        <p>$0.00</p>
+                    </div>
+                    <div className="expenses">
+                    <h4>Income</h4>
+                    <p>$0.00</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    );
 }

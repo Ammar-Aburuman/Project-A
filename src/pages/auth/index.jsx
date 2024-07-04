0a97d8295b7b55401ca4f6a15ab1f81a1e273523
Ammar Aburuman <A.aburuman@protonmail.com>
2024-07-04 14:28:41 +0300

    add trasnfer to expense-tracker website + fix css

--- src/pages/auth/index.jsx
+++ src/pages/auth/index.jsx
@@ -1,11 +1,21 @@
 import {auth, provider} from "../../config/firebase-config"
 import { signInWithPopup } from "firebase/auth"
+import { useNavigate } from "react-router-dom"
 
 export const Auth = () => {
 
+    const navigate = useNavigate();
+
     const signInWithGoogle = async () => {
         const results = await signInWithPopup(auth,provider)
-        console.log(results)
+        const authInfo = {
+            userID : results.user.uid,
+            name : results.displayName,
+            profilePhoto: results.user.photoURL,
+            isAuth: true,
+        }
+        localStorage.setItem("auth",JSON.stringify(authInfo))    
+        navigate("/expense-tracker");
     }
 
     return (
